73b099b6018a9f8b8dd0b743263ed0f3333646d0
Stephanie Viveros <sviveros05@gmail.com>
2020-01-29 20:10:22 -0600

    Add Style to ColorPicker Component

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import styles from './ColorPicker.module.css'
 
 const ColorPicker = (props) => {
 	return (
 		<div>
-			{props.colors.map(color => 
-				<button key={color}>{color}</button>
+			{props.colors.map((color, idx) => 
+				<button 
+				className={styles.button} 
+				key={color}
+				style={idx === props.selColorIdx ? {backgroundColor: 'white', borderColor: color} : {backgroundColor: color, borderColor: color} }/>
 			)}
 		</div>
 	)
